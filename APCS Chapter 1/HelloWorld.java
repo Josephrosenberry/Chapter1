7291ac421e6a6c3964a71ab3f7c70e7a5c1ef365
Josephrosenberry <31011380+Josephrosenberry@users.noreply.github.com>
2017-08-14 09:08:59 -0700

    Add files via upload

---
+++ APCS Chapter 1/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void main (String[] args)
+    {
+        System.out.println  ("Hello, World!");
+    }
+}
+//6. // No error message
+//7. //It is just a message so anything is viable
+//8. // "unclosed string literal"
+//9. // ')' expected
+//10. //';' expected
